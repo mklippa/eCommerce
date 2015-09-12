a1b9d5520e20d35844ae04538aad05b86055448b
Maxim Klippa <maxim.klippa@sibers.com>
2015-09-12 19:48:54 +0600

    add new categories;

--- eCommerce/eCommerce.Core/Repositories/CategoryRepository.cs
+++ eCommerce/eCommerce.Core/Repositories/CategoryRepository.cs
@@ -10,21 +10,86 @@ namespace eCommerce.Core.Repositories
 
         public CategoryRepository()
         {
+            #region [SPORT CATEGORIES]
             var sportCategory = new Category
             {
                 Id = Guid.NewGuid(),
                 Name = "Sport"
             };
+            var sportHockeyCategory = new Category
+            {
+                Id = Guid.NewGuid(),
+                Name = "Hockey"
+            };
+            var sportFootballCategory = new Category
+            {
+                Id = Guid.NewGuid(),
+                Name = "Football"
+            };
+            var sportBasketballCategory = new Category
+            {
+                Id = Guid.NewGuid(),
+                Name = "Basketball"
+            };
+            sportCategory.SubCategories = new List<Category>
+            {
+                sportHockeyCategory, sportFootballCategory, sportBasketballCategory
+            };
+            #endregion
+
+            #region [FOOD CATEGORIES]
             var foodCategory = new Category
             {
                 Id = Guid.NewGuid(),
                 Name = "Food"
             };
+            var foodMeatCategory = new Category
+            {
+                Id = Guid.NewGuid(),
+                Name = "Meat"
+            };
+            var foodPlantCategory = new Category
+            {
+                Id = Guid.NewGuid(),
+                Name = "Plant Food"
+            };
+            var foodConfectionCategory = new Category
+            {
+                Id = Guid.NewGuid(),
+                Name = "Confection"
+            };
+            foodCategory.SubCategories = new List<Category>
+            {
+                foodMeatCategory, foodPlantCategory, foodConfectionCategory
+            };
+            #endregion
+
+            #region [GAME CATEGORY]
             var gameCategory = new Category
             {
                 Id = Guid.NewGuid(),
                 Name = "Game"
             };
+            var gameActionCategory = new Category
+            {
+                Id = Guid.NewGuid(),
+                Name = "Action"
+            };
+            var gameRpgCategory = new Category
+            {
+                Id = Guid.NewGuid(),
+                Name = "RPG"
+            };
+            var gameArcadeCategory = new Category
+            {
+                Id = Guid.NewGuid(),
+                Name = "Arcade"
+            };
+            gameCategory.SubCategories = new List<Category>
+            {
+                gameActionCategory, gameRpgCategory, gameArcadeCategory
+            };
+            #endregion
 
             _categories = new List<Category>
             {
