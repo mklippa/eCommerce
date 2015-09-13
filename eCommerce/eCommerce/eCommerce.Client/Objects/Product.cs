3a3f256095b6949f78653bb47443c47910b93594
Maxim Klippa <maxim.klippa@sibers.com>
2015-09-13 17:16:38 +0600

    add products images;

--- eCommerce/eCommerce/eCommerce.Client/Objects/Product.cs
+++ eCommerce/eCommerce/eCommerce.Client/Objects/Product.cs
@@ -5,6 +5,9 @@ namespace eCommerce.Client.Objects
     public class Product : BaseObject
     {
         public string Name { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
         public Guid CategoryId { get; set; }
 
         public override string ToString()
