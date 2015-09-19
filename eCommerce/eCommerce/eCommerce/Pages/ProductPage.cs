62cbdd02440e489a5da70c8f15b5229e9d8c7a40
Maxim Klippa <maxim.klippa@sibers.com>
2015-09-19 17:12:52 +0600

    finished product page;

--- eCommerce/eCommerce/eCommerce/Pages/ProductPage.cs
+++ eCommerce/eCommerce/eCommerce/Pages/ProductPage.cs
@@ -1,4 +1,6 @@
-﻿using eCommerce.Client.Objects;
+﻿using System.Collections.Generic;
+using eCommerce.Client.Objects;
+using eCommerce.Viewes;
 using Xamarin.Forms;
 
 namespace eCommerce.Pages
@@ -11,10 +13,68 @@ namespace eCommerce.Pages
         {
             _product = product;
             Title = "Product";
+            var grid = new Grid
+            {
+                Padding = new Thickness(5, 0),
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                RowDefinitions = { new RowDefinition { Height = new GridLength(100)} },
+                ColumnDefinitions =
+                {
+                    new ColumnDefinition { Width = new GridLength(100) },
+                    new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) },
+                    new ColumnDefinition { Width = new GridLength(100) }
+                }
+            };
+            grid.Children.Add(new ContentView
+            {
+                Content = new Image
+                {
+                    HeightRequest = 100,
+                    WidthRequest = 100,
+                    Source = ImageSource.FromResource((_product.ImageSource))
+                }
+            }, 0, 0);
+            grid.Children.Add(new ContentView
+            {
+                Content = new Label
+                {
+                    Text = string.Format("${0:0.00}", _product.Price),
+                    TextColor = Color.Red,
+                    FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                    HorizontalOptions = LayoutOptions.CenterAndExpand
+                }
+            }, 1, 0);
+            grid.Children.Add(new ContentView
+            {
+                Content = new Button
+                {
+                    VerticalOptions = LayoutOptions.Start,
+                    BorderWidth = 1,
+                    BorderColor = Color.Gray,
+                    Text = "Add",
+                    Image = "cart.png"
+                }
+            }, 2, 0);
             Content = new StackLayout
             {
+                Spacing = 0,
                 Children = {
-                    new Label { Text = _product.Name }
+                    new ContentView
+                    {
+                        Padding = new Thickness(5),
+                        Content = new Label
+                        {
+                            Text = _product.Name,
+                            FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
+                        },
+                    },
+                    grid,
+                    new ContentView
+                    {
+                        VerticalOptions = LayoutOptions.FillAndExpand,
+                        Padding = new Thickness(5),
+                        Content = new InfoView(_product.Name, _product.Description)
+                    }
                 }
             };
         }
---
+++ eCommerce/eCommerce/eCommerce/Viewes/InfoView.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace eCommerce.Viewes
+{
+    public class InfoView : ContentView
+    {
+        public string HeaderText { get; private set; }
+        public string BodyText { get; private set; }
+
+        public InfoView(string headerText, string bodyText)
+        {
+            HeaderText = headerText;
+            BodyText = bodyText;
+            VerticalOptions = LayoutOptions.FillAndExpand;
+
+            Content = new StackLayout
+            {
+                Spacing = 0,
+                Children = {
+                    new ContentView
+                    {
+                        Padding = new Thickness(5),
+                        BackgroundColor = Color.Red,
+                        Content = new Label
+                        {
+                            Text = HeaderText,
+                            TextColor = Color.White,
+                        }
+                    },
+                    new ContentView
+                    {
+                        Padding = new Thickness(5),
+                        VerticalOptions = LayoutOptions.FillAndExpand,
+                        Content = new Label
+                        {
+                            Text = BodyText,
+                            TextColor = Color.Black,
+                            FontSize = Device.GetNamedSize(NamedSize.Small, typeof(Label))
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
